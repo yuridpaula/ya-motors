920391491f647d3c6a5d7e372b4d56552635f035
Yuri de Paula <yuri.oliveira@democrata.com.br>
2018-08-16 11:03:21 -0300

    incporporação angular material tests

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.css']
+  styleUrls: ['./access.component.css'],
 })
+
 export class AccessComponent implements OnInit {
   
-  isDisabled: boolean = false;
-  clickCounter: number = 0;
-  toggleDisable: boolean = false;
-
-  constructor() { }
+  constructor(){}
 
   ngOnInit() {
+  }
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
   }
 
 }
